e9f4d2a253c135fd97cbd129827c89efae737d4d
Silvio <silvio.dias@etec.sp.gov.br>
2022-03-13 03:09:19 -0300

    feat: Adding routes.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 import { ThemeProvider } from 'styled-components'
 
+import { RoutesApp } from '@routes'
+
 import theme from '@global/styles/theme'
 import { GlobalStyle } from '@global/styles/global'
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <h1>Hello world</h1>
+      <RoutesApp />
 
       <GlobalStyle />
     </ThemeProvider>
