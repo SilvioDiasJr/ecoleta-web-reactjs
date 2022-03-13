cab4c5a8ef2de0ce95c1c3504c52c9e299f7df54
Silvio <silvio.dias@etec.sp.gov.br>
2022-03-13 02:48:57 -0300

    feat: Adding fonts and colors.

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,4 +1,22 @@
 export default {
-  colors: {},
-  fonts: {}
+  colors: {
+    main: '#34CB79',
+    main_dark: '#2FB86E',
+    secondary: '#E1FAEC',
+
+    white: '#FFFFFF',
+
+    title: '#322153',
+    text: '#6C6C80',
+
+    input_placeholder: '#A0A0B2',
+
+    card_background: '#FFFFFF',
+
+    background: '#F0F0F5'
+  },
+  fonts: {
+    bold: 'Ubuntu',
+    regular: 'Roboto'
+  }
 }
